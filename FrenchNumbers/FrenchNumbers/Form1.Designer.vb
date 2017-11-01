639505101b69dc07b6222e9cff9dd44a05a6444f
arichards558 <31348479+arichards558@users.noreply.github.com>
2017-11-01 10:17:17 -0500

    Update FrenchNumbers

---
+++ FrenchNumbers/FrenchNumbers/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.lblMessageTwo = New System.Windows.Forms.Label()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMessage.Location = New System.Drawing.Point(31, 9)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(357, 16)
+        Me.lblMessage.TabIndex = 0
+        Me.lblMessage.Text = "Do you know the French words for the numbers 1 through 5?"
+        '
+        'lblMessageTwo
+        '
+        Me.lblMessageTwo.AutoSize = True
+        Me.lblMessageTwo.Location = New System.Drawing.Point(112, 49)
+        Me.lblMessageTwo.Name = "lblMessageTwo"
+        Me.lblMessageTwo.Size = New System.Drawing.Size(178, 13)
+        Me.lblMessageTwo.TabIndex = 1
+        Me.lblMessageTwo.Text = "Click the buttons below to see them!"
+        '
+        'btnOne
+        '
+        Me.btnOne.Location = New System.Drawing.Point(34, 95)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(75, 23)
+        Me.btnOne.TabIndex = 2
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(170, 95)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnTwo.TabIndex = 3
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Location = New System.Drawing.Point(304, 95)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(75, 23)
+        Me.btnThree.TabIndex = 4
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Location = New System.Drawing.Point(34, 184)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(75, 23)
+        Me.btnFour.TabIndex = 5
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Location = New System.Drawing.Point(170, 184)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(75, 23)
+        Me.btnFive.TabIndex = 6
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(304, 184)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Controls.Add(Me.lblMessageTwo)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+    Friend WithEvents lblMessageTwo As System.Windows.Forms.Label
+    Friend WithEvents btnOne As System.Windows.Forms.Button
+    Friend WithEvents btnTwo As System.Windows.Forms.Button
+    Friend WithEvents btnThree As System.Windows.Forms.Button
+    Friend WithEvents btnFour As System.Windows.Forms.Button
+    Friend WithEvents btnFive As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ FrenchNumbers/FrenchNumbers/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblMessage.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblMessageTwo.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+        MessageBox.Show("Trois")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+        MessageBox.Show("Cinq")
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        MessageBox.Show("Deux")
+    End Sub
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        MessageBox.Show("Un")
+    End Sub
+
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+        MessageBox.Show("Qautre")
+    End Sub
+End Class
